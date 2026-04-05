ae3cac07d47ff950c7565c704cb6e2aa4b0fd82d
Landree Allen <leallen@smith.edu>
2026-04-05 12:54:19 -0400

    cafe?

--- CampusMap.java
+++ CampusMap.java
@@ -3,10 +3,12 @@ import java.util.ArrayList;
 public class CampusMap {
 
     ArrayList<Building> buildings;
+    ArrayList<Cafe> cafes;
 
     /* Default constructor, initializes empty ArrayList */
     public CampusMap() {
         buildings = new ArrayList<Building>();
+        cafes = new ArrayList<Cafe>();
     }
 
     /**
@@ -16,6 +18,12 @@ public class CampusMap {
     public void addBuilding(Building b) {
         System.out.println("Adding building...");
         buildings.add(b);
+        System.out.println("-->Successfully added " + b.getName() + " to the map.");
+    }
+
+    public void addCafe(Cafe b) {
+        System.out.println("Adding Cafe...");
+        cafes.add(b);
         System.out.println("-->Successfully added " + b.getName() + " to the map.");
     }
 
@@ -30,6 +38,12 @@ public class CampusMap {
         System.out.println("-->Successfully removed " + b.getName() + " to the map.");
         return b;
     }
+    public Cafe removeCafe(Cafe b){
+        System.out.println("Removing Cafe...");
+        cafes.remove(b);
+        System.out.println("-->Successfully removed " + b.getName() + " to the map.");
+        return b;
+    }
 
     public String toString() {
         String mapString = "DIRECTORY of BUILDINGS";
@@ -40,10 +54,12 @@ public class CampusMap {
         return mapString;
     }
 
+
     public static void main(String[] args) {
         CampusMap myMap = new CampusMap();
         myMap.addBuilding(new Building("Ford Hall", "100 Green Street Northampton, MA 01063", 4));
         myMap.addBuilding(new Building("Bass Hall", "4 Tyler Court Northampton, MA 01063", 4));
+        myMap.addCafe(new Cafe("Compass Cafe", "7 Elm St, Northampton, MA 01063"));
         System.out.println(myMap);
     }
     
