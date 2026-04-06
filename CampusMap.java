9e96e0c66cc87086d9839cbda0eb0715797d1515
Landree Allen <leallen@smith.edu>
2026-04-06 10:41:00 -0400

    display

--- CampusMap.java
+++ CampusMap.java
@@ -3,14 +3,10 @@ import java.util.ArrayList;
 public class CampusMap {
 
     ArrayList<Building> buildings;
-    ArrayList<Cafe> cafes;
-    ArrayList<House> houses;
 
     /* Default constructor, initializes empty ArrayList */
     public CampusMap() {
         buildings = new ArrayList<Building>();
-        cafes = new ArrayList<Cafe>();
-        houses = new ArrayList<House>();
     }
 
     /**
@@ -22,13 +18,6 @@ public class CampusMap {
         buildings.add(b);
         System.out.println("-->Successfully added " + b.getName() + " to the map.");
     }
-
-    public void addCafe(Cafe b) {
-        System.out.println("Adding Cafe...");
-        cafes.add(b);
-        System.out.println("-->Successfully added " + b.getName() + " to the map.");
-    }
-
     /**
      * Removes a Building from the map
      * @param b the Building to remove
@@ -40,13 +29,7 @@ public class CampusMap {
         System.out.println("-->Successfully removed " + b.getName() + " to the map.");
         return b;
     }
-    public Cafe removeCafe(Cafe b){
-        System.out.println("Removing Cafe...");
-        cafes.remove(b);
-        System.out.println("-->Successfully removed " + b.getName() + " to the map.");
-        return b;
-    }
-
+    
     public String toString() {
         String mapString = "DIRECTORY of BUILDINGS";
 
@@ -59,10 +42,35 @@ public class CampusMap {
 
     public static void main(String[] args) {
         CampusMap myMap = new CampusMap();
-        myMap.addBuilding(new Building("Ford Hall", "100 Green Street Northampton, MA 01063", 4));
-        myMap.addBuilding(new Building("Bass Hall", "4 Tyler Court Northampton, MA 01063", 4));
-        myMap.addCafe(new Cafe("Compass Cafe", "7 Elm St, Northampton, MA 01063"));
+
+        Building[] smithBuildings = {
+            new Building("Ford Hall", "100 Green Street Northampton, MA 01063"),
+            new Building("Bass Hall", "4 Tyler Court Northampton, MA 01063", 4),
+            new Building("Seelye Hall", "2 Seelye Drive Northampton, MA 01063", 3),
+            new Library("Neilson Library", "7 Neilson Drive Northampton, MA 01063", 4, true),
+            new Library("Hillyer Library", "20 Elm Street Northampton, MA 01063", 2, false),
+            new Cafe("Compass Cafe", "7 Elm St Northampton, MA 01063"),
+            new Cafe("CC Cafe", "100 Elm Street Northampton, MA 01063", 3),
+            new House("Chapin House", "11 Elm Street Northampton, MA 01063"),
+            new House("Washburn House", "4 Seelye Dr, Northampton, MA 01063", 4, false, false),
+            new House("Lawrence House", "99 Green St, Northampton, MA 01060", 4, true, false)
+        };
+
+        for (Building b : smithBuildings) {
+            myMap.addBuilding(b);
+        }
+        // demonstrate House overloaded constructor (name + address only)
+        House scales = new House("Scales House", "Elm Street Northampton, MA 01063");
+        myMap.addBuilding(scales);
+
+        // demonstrate Library overloaded constructor (name only)
+        Library smallLib = new Library("Small Library", "5 Green Street");
+        myMap.addBuilding(smallLib);
+
+        // demonstrate Cafe overloaded constructor (name + address + floors)
+        Cafe campusCafe = new Cafe("Campus Cafe", "10 Green Street", 1);
+        myMap.addBuilding(campusCafe);
+
         System.out.println(myMap);
     }
-    
-}
+    }
